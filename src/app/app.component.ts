eb9ab687d34638241f4ab3a0280e39a14dc96077
AbidSaleem <abidsaleem@live.com>
2022-02-06 16:35:59 +0300

    Bubble Sort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 import { Algorithms } from './enums/algorithms-enums';
 
 @Component({
@@ -6,10 +6,11 @@ import { Algorithms } from './enums/algorithms-enums';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
   title = 'SortingVisualizer';
 
   barHeights: number[] = [];
+  barHeightsBackup: number[] = [];
   length = 400;
   size = 80;
   algorithm = Algorithms.NONE;
@@ -18,6 +19,10 @@ export class AppComponent {
     for(let i = 0; i < this.size; i++) {
       this.barHeights.push(Math.round(Math.random() * this.length + 40));
     }
+  }
+
+  ngOnChanges(): void {
+      console.log("on changes");
   }
 
   algorithmSelected(algorithm: string): void {
@@ -37,4 +42,76 @@ export class AppComponent {
       this.algorithm = Algorithms.QUICK_SORT;
     }
   }
+
+  playClicked(): void {
+    if(this.algorithm === Algorithms.BUBBLE_SORT) {
+      this.bubbleSort();
+    }
+    else if(this.algorithm === Algorithms.SELECTION_SORT) {
+      this.selectionSort();
+    }
+    else if(this.algorithm === Algorithms.INSERTION_SORT) {
+      this.insertionSort();
+    }
+    else if(this.algorithm === Algorithms.MERGE_SORT) {
+      this.mergeSort();
+    }
+    else if(this.algorithm === Algorithms.QUICK_SORT) {
+      this.quickSort();
+    }
+  }
+
+  async bubbleSort() {
+    this.barHeightsBackup = [...this.barHeights];
+
+    for(let i = 0; i < this.size - 1; i++) {
+      for(let j = 0; j < this.size - i - 1; j++) {
+
+        let element1 = document.getElementsByClassName('' + j)[0] as HTMLElement;
+        element1.style.backgroundColor = '#B8405E';
+
+        await this.delay(10);
+
+        if(this.barHeights[j] > this.barHeights[j + 1]) {
+          let temp = this.barHeights[j];
+          this.barHeights[j] = this.barHeights[j + 1];
+          this.barHeights[j + 1] = temp;
+
+          await this.delay(10);
+        }
+
+        element1.style.backgroundColor = '#35858B';
+
+      }
+    }
+  }
+
+  selectionSort() {
+    console.log('selection sort');
+
+  }
+
+  insertionSort() {
+    console.log('insertion sort');
+
+  }
+
+  mergeSort() {
+    console.log('merge sort');
+
+  }
+
+  quickSort() {
+    console.log('quick sort');
+
+  }
+
+  reset() {
+    this.barHeights = [...this.barHeightsBackup];
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
 }
